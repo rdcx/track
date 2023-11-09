8c046083ba8f0420e51a6068cc44e388ec50cb2a
Ross Chater <rdchater@gmail.com>
2023-11-09 17:35:50 +0000

    Test path

--- api/router/router.go
+++ api/router/router.go
@@ -27,7 +27,7 @@ func SetUpRouter(pubAddr string) *gin.Engine {
 	r.GET("/hits/:domain", handler.Hits)
 	r.GET("/tracker", handler.Tracker)
 
-	r.LoadHTMLGlob("templates/**/*")
+	r.LoadHTMLGlob("../templates/**/*")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.tmpl", nil)
